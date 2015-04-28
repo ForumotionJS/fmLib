9fe968ad8ab709d72b3f9ae572ed5d735059ec25
Siti Almeida Maryam <horpdorpclop@gmail.com>
2015-04-28 16:20:05 +0800

    Create fmLib-jol.js

---
+++ fmLib-jol.js
@@ -0,0 +1,19 @@
+/* 
+* fmLib 
+* Copyright to ForumotionJS at GitHub 
+*/
+
+/* Wrapped in JOL (JavaScript Object Literal) to avoid unwanted issues regarding the code */
+var fmLib = {
+   localData: {
+    "user_level" = _userdata.user_level,
+    "user_id" = _userdata.user_id,
+    "user_lang" = _userdata.user_lang,
+    "user_privmgs" = _userdata.user_nb_privmsg,
+    "user_posts" = _userdata.user_posts,
+    "user_avatar" = _userdata.avatar,
+    "user_constructor" = _userdata.constructor,
+    "user_reps" = _userdata.point_reputation,
+    "user_toolbar" = _userdata.active_toolbar
+  }
+}
